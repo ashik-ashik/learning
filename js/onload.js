6ca31f5fe66a05e9c3dd41360a74218377ea5744
Md Ashik Ali <mdashika989@yahoo.com>
2021-05-18 15:29:25 +0600

    alert added

--- js/onload.js
+++ js/onload.js
@@ -1,23 +1,5 @@
 
-let bdy = document.querySelector('#body');
-    bdy.style.background = "#f00 !important";
-    bdy.style.visibility = "hidden";
-
-    // onload action
-    window.onload = function(){
-        let ok = confirm("Are You Sure to visit This site???")
-        if(ok == true){
-            let verify = String(prompt("Provide the keyword to visit this site"));
-                verify = verify.toLowerCase();
-            if(verify == "local"){
-                bdy.style.visibility = "visible";
-            }
-            else{
-                window.location = "pages/404.html"
-            }
-        }else{
-            window.location = "pages/404.html"
-        }
+    let user = document.querySelector("#user");let alertMessage = document.querySelector(".bosx"); alertMessage.style.visibility = "hidden"; 
+    window.onload = function(){let ok = confirm("Are You Sure to visit This site???");if(ok == true){let getUserName = prompt("Please Provide Your Name:");if((getUserName != '')){ user.textContent = getUserName.toUpperCase(); alertMessage.style.visibility = "visible";}else{window.location="pages/404.html";}}else{window.location="pages/404.html";}
 };
-
 
